be0a0006dec3dd859329731c9fda2b3b188fc811
Gustavo Reis <reisgg9@gmail.com>
2024-04-04 22:55:41 -0300

    apenas um coment

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
 window.alert('Diga a frase mágica.');
 window.confirm('Está gostando de JS?');
 window.prompt('Qual é seu nome?');
+
+// variaveis em JAVASCRIPT - Nessa aula o Guanabara explicou o básico pra quem não sabe.
